be5ebf253272f51f373681a048e0d634955e3dbd
Gohime Hatake <146865909+Elohime7@users.noreply.github.com>
2024-08-04 02:31:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,11 +7,11 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
-global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.ownername = "ᏒᎽᎠᎬᏒ ᏰᏝᎯᏃᎬ";
+global.owner = ['243980635216'];
+global.premium = ['243980635216'];
+global.packname = "ᏒᎽᎠᎬᏒ ᏰᏝᎯᏃᎬ";
+global.author = "ᏒᎽᎠᎬᏒ ᏰᏝᎯᏃᎬ";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
